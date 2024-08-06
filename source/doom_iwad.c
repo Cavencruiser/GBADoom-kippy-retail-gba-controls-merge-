c257dd4388da631f0cba83994aa1bcbd968a552a
Cavencruiser <93107023+Cavencruiser@users.noreply.github.com>
2024-08-06 16:55:26 -0400

    iwad

--- source/doom_iwad.c
+++ source/doom_iwad.c
@@ -1,13 +1,9 @@
 #pragma GCC optimize ("-O0")
+
+#include "iwad/freedoom.c"
+//#include "iwad/freedoom2.c"
+
 #include "doom_iwad.h"
 
-//Uncomment which edition you want to compile
-#include "iwad/doom1.c"
-//#include "iwad/doom.c"
-//#include "iwad/doomu.c"
-//#include "iwad/doom2.c"
-//#include "iwad/tnt.c"
-//#include "iwad/plutonia.c"
-//#include "iwad/sigil.c"
-
 const unsigned int doom_iwad_len = sizeof(doom_iwad);
+
